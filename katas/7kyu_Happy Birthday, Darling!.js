cee73057de78c0b333d529cf83dc8219a1145ff5
Alyssa-Ng <38221362+Alyssa-Ng@users.noreply.github.com>
2020-07-09 22:48:12 +0800

    Added 7kyu_Happy Birthday, Darling! Solution

---
+++ katas/7kyu_Happy Birthday, Darling!.js
@@ -0,0 +1,3 @@
+//https://www.codewars.com/kata/5e96332d18ac870032eb735f
+
+var womensAge = (n) => n % 2 === 0 ?  `${n}? That's just 20, in base ${n/2}!` : `${n}? That's just 21, in base ${(n-1)/2}!` ;
