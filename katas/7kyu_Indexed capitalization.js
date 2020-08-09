b81371bfcce297c9cf663e010967487ce132a9ff
Alyssa-Ng <38221362+Alyssa-Ng@users.noreply.github.com>
2020-08-09 23:39:42 +0800

    Added 7kyu_Indexed capitalization Solution

---
+++ katas/7kyu_Indexed capitalization.js
@@ -0,0 +1,7 @@
+//https://www.codewars.com/kata/59cfc09a86a6fdf6df0000f1
+
+function capitalize(s,arr){
+  let result = s.split('');
+  for(int of arr) int > result.length ? null : result.splice(int , 1, s[int].toUpperCase());
+  return result.join('');
+}
